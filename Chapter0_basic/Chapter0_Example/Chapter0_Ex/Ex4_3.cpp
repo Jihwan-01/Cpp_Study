5996253cfcdd9ca92c0eb11c635943c24da3c2be
Jihwan-01 <jihwanj21@naver.com>
2021-02-02 20:07:16 +0900

    반복문 do-while

---
+++ Chapter0_basic/Chapter0_Example/Chapter0_Ex/Ex4_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i, a, b, sum = 0;
+	cout << "두 개의 정수를 입력하세요>>";
+	cin >> a >> b;
+
+	i = a;
+	do {
+		sum += i;
+		i++;
+	} while (i <= b); // i가 b보다 작거나 같은 동안 반복
+	cout << a << "부터 " << b << "까지 합은 " << sum << "이다.";
+}
