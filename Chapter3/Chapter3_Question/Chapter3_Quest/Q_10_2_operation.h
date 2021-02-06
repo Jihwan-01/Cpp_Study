7317819d220f16146a02792c844bbf4b1da6ec9f
Jihwan-01 <jihwanj21@naver.com>
2021-02-06 13:45:32 +0900

    사칙연산 클래스: 클래스의 선언부와 구현부 분리

---
+++ Chapter3/Chapter3_Question/Chapter3_Quest/Q_10_2_operation.h
@@ -0,0 +1,31 @@
+/*
+본 소스 파일(Q_10_2.h)은 Q_10_1.cpp 파일에서 파일 분리를 수행한 파일임.
+*/
+
+class Add {
+	int a, b;
+public:
+	void setValue(int x, int y);
+	int calculate();
+};
+
+class Sub {
+	int a, b;
+public:
+	void setValue(int x, int y);
+	int calculate();
+};
+
+class Mul {
+	int a, b;
+public:
+	void setValue(int x, int y);
+	int calculate();
+};
+
+class Div {
+	int a, b;
+public:
+	void setValue(int x, int y);
+	int calculate();
+};
